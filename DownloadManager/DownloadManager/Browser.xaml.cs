cfbcb226c93f3cf401b8110abb790a9d7a47a857
DESKTOP-GE8MQ17\Maskarade <pattkhwela@outlook.com>
2017-01-15 00:44:17 +0200

    Initial commit of all working files in project solution

---
+++ DownloadManager/DownloadManager/Browser.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DownloadManager
+{
+    /// <summary>
+    /// An empty page that can be used on its own o r navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Browser : Page
+    {
+        Library Library = new Library();
+
+        public Browser()
+        {
+            this.InitializeComponent();
+        }
+        private void Value_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            Library.Go(ref Display, Value.Text, e);
+        }
+
+        
+        private void Display_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            if(args.IsSuccess)
+            {
+                Value.Text = args.Uri.ToString(); 
+            }
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Library.Back(ref Display);
+        }
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            Library.Forward(ref Display);
+        }
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Refresh();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
+
---
+++ DownloadManager/DownloadManager/Library.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace DownloadManager
+{
+    public class Library
+    {
+        public void Back(ref WebView web)
+        {
+            if (web.CanGoBack)
+            {
+                web.GoBack();
+
+            }
+
+        }
+        public void Forward(ref WebView web)
+        {
+            if (web.CanGoForward)
+            {
+                web.GoForward();
+            }
+        }
+        public void Go(ref WebView web, string value, KeyRoutedEventArgs args)
+        {
+            if(args.Key == Windows.System.VirtualKey.Enter)
+            {
+                try
+                {
+                    web.Navigate(new Uri(value));
+                }
+                catch 
+                {
+                    
+                }
+                web.Focus(Windows.UI.Xaml.FocusState.Keyboard);
+            }
+        }
+
+       
+    }
+}
